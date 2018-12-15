b55636bb1e6cf07f875eb125a9ad71daaae2ebec
v-ilin <xrigikx@mail.ru>
2018-12-15 12:12:12 +0100

    Added getting images and bounding boxes from dataset

--- train.py
+++ train.py
@@ -43,6 +43,9 @@ def get_kvp_from_file(filepath):
     kvp = {}
     with open(filepath) as file:
         for line in file:
+            line = line.replace('\r\n', '')
+            line = line.replace('\n', '')
+
             key, value = line.partition("=")[::2]
             kvp[key.strip()] = value
 
@@ -53,20 +56,81 @@ def get_sub_dirs(folder):
     return next(os.walk(folder))[1]
 
 
+def compose_img_filepath(dir, img_id, img_ext):
+    img_id_str = str(int(img_id)).zfill(6)
+    img_filename = img_id_str + img_ext
+    img_filepath = os.path.join(dir, img_filename)
+
+    return img_filepath
+
+
+def get_bbox_coord(csv_row):
+    x1 = int(csv_row[2])
+    y1 = int(csv_row[3])
+
+    width = int(csv_row[4])
+    height = int(csv_row[5])
+
+    x2 = x1 + width
+    y2 = y1 + height
+
+    return (x1, y1), (x2, y2)
+
+
 def get_batch(root_dataset_dir):
     subdirs = get_sub_dirs(root_dataset_dir)
-    print(subdirs)
+
     for seq_dir_name in subdirs:
         seq_dir_path = os.path.join(root_dataset_dir, seq_dir_name)
         seq_info_filepath = os.path.join(seq_dir_path, 'seqinfo.ini')
         seq_info = get_kvp_from_file(seq_info_filepath)
 
-        images_dir = os.path.join(seq_dir_path)
+        images_dir = os.path.join(seq_dir_path, seq_info['imDir'])
         y_filepath = os.path.join(seq_dir_path, 'gt', 'gt.txt')
+        y_df = pd.read_csv(y_filepath, header=None)
 
-        print(seq_info['imDir'])
-        print(seq_info['imExt'])
-        print(images_dir)
+        y_df_grouped = y_df.groupby(1)
+
+        for name, group in y_df_grouped:
+            for index, row in group.iterrows():
+                target_row_index = index + 1
+
+                if target_row_index >= group.shape[0]:
+                    continue
+
+                target_row = group.take([target_row_index])
+
+                img_filepath = compose_img_filepath(images_dir, row[0], seq_info['imExt'])
+                target_img_filepath = compose_img_filepath(images_dir, target_row[0], seq_info['imExt'])
+
+                img = cv2.imread(img_filepath)
+                target_img = cv2.imread(target_img_filepath)
+
+                # img = cv2.resize(img, (IMG_HEIGHT, IMG_WIDTH))
+                # target_img = cv2.resize(target_img, (IMG_HEIGHT, IMG_WIDTH))
+
+                img_bbox_left_bottom, img_bbox_right_top = get_bbox_coord(row)
+                target_img_bbox_left_bottom, target_img_bbox_right_top = get_bbox_coord(target_row)
+
+                # return (img, img_bbox), (target_img, target_img_bbox)
+
+                cv2.rectangle(img, img_bbox_left_bottom, img_bbox_right_top, (255,0,0), 2)
+                cv2.imwrite('img_with_bbox.jpg', img)
+
+                cv2.rectangle(target_img, target_img_bbox_left_bottom, target_img_bbox_right_top, (255,0,0), 2)
+                cv2.imwrite('target_img_with_bbox.jpg', target_img)
+
+                print('img_filepath = {}'.format(img_filepath))
+                print('target_img_filepath = {}'.format(target_img_filepath))
+                print('img.shape = {}'.format(img.shape))
+                print('target_img.shape = {}'.format(target_img.shape))
+                break
+            break
+
+            print('object {}, frame count = {}'.format(name, group.shape[0]))
+            # print(group)
+        # print(y_df_grouped)
+        break
 
 
 def main():
@@ -92,8 +156,6 @@ def main():
     net.blobs['image'].data[...] = image_input
     net.blobs['target'].data[...] = target_input
     net.blobs['bbox'].data[...] = bbox_input
-
-    print(net.blobs['bbox'].data.shape)
 
     # print(net.blobs['out'].data)
 
